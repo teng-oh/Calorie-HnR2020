62c71d24e79823feb39ac9c703e979241a716be4
teng-oh <juntengoh@gmail.com>
2020-01-18 17:01:00 +0800

    more testing

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
+data = pd.read_csv("static\data\\food-items.csv")
+data_dict = {col: list(data[col]) for col in data.columns}
 
 @app.route("/", methods = ['GET','POST'])
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        try:
+            calories = data_dict.get(request.form)
+        except:
+            calories = ""
+        return render_template("home.html", response = calories)
+    else:
+        return render_template("home.html")
     
 @app.route("/about")
 def about():
